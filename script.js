f95841e77451f211ecd0cdc6132c031aa8da862e
DexterWurnWSU <43418885+DexterWurnWSU@users.noreply.github.com>
2022-03-05 11:00:39 -0500

    basic files
    basic JS that grabs info from form submission and the html file that I used for testing

---
+++ script.js
@@ -0,0 +1,7 @@
+function main()
+{
+	var fname = document.getElementById("fname").value;
+	var lname = document.getElementById("lname").value;
+	document.write("First Name = " + fname + " Last Name = " + lname);
+	
+}
